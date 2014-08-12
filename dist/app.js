65cfb29f1dd34449b21c0ed7add14fa82504030a
Karlis Venters <karlis.venters@accenture.com>
2014-08-12 09:37:40 +0300

    Initial commit

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+/*! app - v - 2014-08-11
+* Copyright (c) 2014 ; Licensed  */
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
